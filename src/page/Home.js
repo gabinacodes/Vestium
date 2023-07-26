cf549abf19a2d0558962dbc967ca77d15fede263
gabinacodes <badruraidah@gmail.com>
2023-07-26 22:13:41 +0100

    hover effect last

--- src/page/Home.js
+++ src/page/Home.js
@@ -10,7 +10,7 @@ function Home() {
           <p className='text-lg w-9 font-normal pt-2'>Accepted Bookings </p>
 
         </div>
-        <div className='m-2 p-3 bg-sand rounded-lg text-white cursor-pointer hover:bg-macaroon' >
+        <div className='m-2 p-3 bg-sand rounded-lg text-white cursor-pointer hover:bg-yellow-600' >
           <img alt='img' src={Image.ali} />
           <p className='text-xl  pt-3 font-normal'>0</p>
           <p className='text-lg w-9 font-normal pt-2'>Pending Bookings</p>
