e2597554bcc24a1c19f658ab35a22f30d4fa983a
gabinacodes <badruraidah@gmail.com>
2023-07-26 14:58:43 +0100

    rr

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "tailwindcss/tailwind.css"
+import Home from './page/Home'
 function App() {
   return (
     <div className="App"> 
-       helo 
+       <Home/>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens:{
+      sm:'320px',
+      md:'768px',
+      lg:'980px',
+      xl:'1440px'
+    },
+    extend: {
+      colors:{
+        babyBlue:'rgb(137,207,240)',
+        royalBlue: '	rgb(65,105,225)',
+        teal:'rgb(0,128,128)',
+        turquoise:'rgb(64,224,208)',
+        macaroon:'RGB(179,139,113)',
+        fire:' RGB(143,63,42)',
+        sand:'rgb(194, 178, 128)',
+        buttermilk:'rg(255, 246, 186)'
+      }
+    },
+
   },
   plugins: [],
 }
