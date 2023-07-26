066a80e0c84521466fbe79f2a652bcab3b261f6f
gabinacodes <badruraidah@gmail.com>
2023-07-26 21:47:05 +0100

    adding cursor

--- src/page/Home.js
+++ src/page/Home.js
@@ -4,23 +4,23 @@ function Home() {
   return (
     <div>
       <div className='flex   flex-wrap m-3 p-3'>
-        <div className=' m-2 p-3 bg-babyBlue rounded-lg text-white' >
+        <div className=' m-2 p-3 bg-babyBlue rounded-lg text-white cursor-pointer' >
           <img alt='img' src={Image.ali} />
           <p className='text-xl  pt-3 font-normal'>0</p>
           <p className='text-lg w-9 font-normal pt-2'>Accepted Bookings </p>
 
         </div>
-        <div className='m-2 p-3 bg-sand rounded-lg text-white' >
+        <div className='m-2 p-3 bg-sand rounded-lg text-white cursor-pointer' >
           <img alt='img' src={Image.ali} />
           <p className='text-xl  pt-3 font-normal'>0</p>
           <p className='text-lg w-9 font-normal pt-2'>Pending Bookings</p>
         </div>
-        <div className='m-2 p-3 bg-teal rounded-lg text-white' >
+        <div className='m-2 p-3 bg-teal rounded-lg text-white cursor-pointer' >
           <img alt='img' src={Image.ali} />
           <p className='text-xl  pt-3 font-normal'>0</p>
           <p className='text-lg w-9 font-normal pt-2'>Rejected Bookings</p>
         </div>
-        <div className=' p-3 m-2 rounded-lg bg-fire text-white'>   
+        <div className=' p-3 m-2 rounded-lg bg-fire text-white cursor-pointer'>   
           <img alt='img' src={Image.ali} />
           <p className='text-xl pt-3 font-normal'>0</p>
           <p className='text-lg w-9 font-normal pt-2'>Completed Bookings </p>
