c60e1daee7143ddb4b674170098f943b7ba41995
gabinacodes <badruraidah@gmail.com>
2023-08-02 14:39:48 +0100

    bb

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,31 +1,13 @@
 import React from 'react'
 import Image  from '../images/imaage'
 function Home() {
+
   return (
     <div>
-      <div className='flex   flex-wrap m-3 p-3'>
-        <div className=' m-2 p-3 bg-babyBlue rounded-lg text-white cursor-pointer hover:bg-sky-950' >
-          <img alt='img' src={Image.ali} />
-          <p className='text-xl  pt-3 font-normal'>0</p>
-          <p className='text-lg w-9 font-normal pt-2'>Accepted Bookings </p>
-
-        </div>
-        <div className='m-2 p-3 bg-sand rounded-lg text-white cursor-pointer hover:bg-yellow-600' >
-          <img alt='img' src={Image.ali} />
-          <p className='text-xl  pt-3 font-normal'>0</p>
-          <p className='text-lg w-9 font-normal pt-2'>Pending Bookings</p>
-        </div>
-        <div className='m-2 p-3 bg-teal rounded-lg text-white cursor-pointer hover:bg-emerald-950 ' >
-          <img alt='img' src={Image.ali} />
-          <p className='text-xl  pt-3 font-normal'>0</p>
-          <p className='text-lg w-9 font-normal pt-2'>Rejected Bookings</p>
-        </div>
-        <div className=' p-3 m-2 rounded-lg bg-fire text-white cursor-pointer hover:bg-amber-950'>   
-          <img alt='img' src={Image.ali} />
-          <p className='text-xl pt-3 font-normal'>0</p>
-          <p className='text-lg w-9 font-normal pt-2'>Completed Bookings </p>
-       </div>
-      </div>
+      <div className='border-solid border-2 border-slate-500 box-content w-4/5 px-6 py-3 rounded-md cursor-text mobile:w-96' id='mail'>
+        <input className='focus:outline-none w-100%' type='text' placeholder='E-mail address'/>
+        <span className='hidden' id='mail_label'>E-mail address</span>
+    </div>
     </div>
   )
 }
