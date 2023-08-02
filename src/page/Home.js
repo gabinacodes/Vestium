d768e1d0a400745f8cb09a8ff566cd802e7ef521
gabinacodes <badruraidah@gmail.com>
2023-08-02 14:41:04 +0100

    bb

--- src/page/Home.js
+++ src/page/Home.js
@@ -3,7 +3,7 @@ import Image  from '../images/imaage'
 function Home() {
 
   return (
-    <div>
+    <div className='flex justify-center ali'>
       <div className='border-solid border-2 border-slate-500 box-content w-4/5 px-6 py-3 rounded-md cursor-text mobile:w-96' id='mail'>
         <input className='focus:outline-none w-100%' type='text' placeholder='E-mail address'/>
         <span className='hidden' id='mail_label'>E-mail address</span>
