23d1c8ce244c7caa5430be635957f5a4ba26fc91
gabinacodes <badruraidah@gmail.com>
2023-08-02 23:09:58 +0100

    rr

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import Image  from '../images/imaage'
+import React from 'react' 
 function Home() {
 
   return (
-    <div className='flex justify-center ali'>
+    <div className='flex justify-center mt-4'  >
       <div className='border-solid border-2 border-slate-500 box-content w-4/5 px-6 py-3 rounded-md cursor-text mobile:w-96' id='mail'>
         <input className='focus:outline-none w-100%' type='text' placeholder='E-mail address'/>
-        <span className='hidden' id='mail_label'>E-mail address</span>
+        <span className='relative text-sm bg-white -top-6 p-3 text-royalBlue right-40 pb-0 animate-bounce' id='mail'>E-mail address</span>
     </div>
     </div>
   )
